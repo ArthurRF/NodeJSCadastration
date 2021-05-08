47379a086f4071d061e5b095a062b719afbd7391
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-05-08 11:16:33 -0300

    Simple changes

--- TypeProject/src/server.ts
+++ TypeProject/src/server.ts
@@ -6,6 +6,12 @@ app.get("/", (request, response) => {
     return response.json({ message : "Hello world"});
 })
 
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+
+    response.json({ name });
+})
+
 app.listen(3333, () => {
     console.log('Server is running');
 })
