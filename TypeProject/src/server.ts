2c534c6ae8f72ae3b165b19d6838dba01b92a34d
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-05-10 19:54:12 -0300

    Folders organization and added Specifications Routes

--- TypeProject/src/server.ts
+++ TypeProject/src/server.ts
@@ -1,21 +1,14 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specificationsRoutes";
 
 const app = express();
 
 app.use(express.json());
+
 app.use("/categories", categoriesRoutes);
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello world" });
-});
-
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-
-    response.json({ name });
-});
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => {
     console.log("Server is running");
