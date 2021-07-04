86c89c102134925ca2bfda9d58e478d079e05a85
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-07-04 16:11:27 -0300

    Alter docs

---
+++ letscode/meu-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App';
+import App2 from './components/App2';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App2 title="Título">
+
+
+    </App2>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
