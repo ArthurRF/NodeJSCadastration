10536a56edaef53f895ef81bbc802ecfbe7bdd84
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-12-21 22:44:04 -0300

    Add S3 Upload

--- TypeProject/src/shared/container/providers/index.ts
+++ TypeProject/src/shared/container/providers/index.ts
@@ -1,9 +1,14 @@
 import { container } from "tsyringe";
 
-import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
+import { IDateProvider } from "./DateProvider/IDateProvider";
+
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
@@ -14,3 +19,13 @@ container.registerInstance<IMailProvider>(
     "EtherealMailProvider",
     new EtherealMailProvider()
 );
+
+const diskStorage = {
+    local: LocalStorageProvider,
+    s3: S3StorageProvider
+}
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    diskStorage[process.env.disk]
+)
