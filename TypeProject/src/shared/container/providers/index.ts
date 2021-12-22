40d8539533e8e18663b4350b0ba2f76524e90b2f
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-12-22 13:43:07 -0300

    Add babel for build

--- TypeProject/src/shared/container/providers/index.ts
+++ TypeProject/src/shared/container/providers/index.ts
@@ -1,31 +1,3 @@
-import { container } from "tsyringe";
-
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { IDateProvider } from "./DateProvider/IDateProvider";
-
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
-import { IStorageProvider } from "./StorageProvider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>(
-    "DayjsDateProvider",
-    DayjsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-    "EtherealMailProvider",
-    new EtherealMailProvider()
-);
-
-const diskStorage = {
-    local: LocalStorageProvider,
-    s3: S3StorageProvider
-}
-
-container.registerSingleton<IStorageProvider>(
-    "StorageProvider",
-    diskStorage[process.env.disk]
-)
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
